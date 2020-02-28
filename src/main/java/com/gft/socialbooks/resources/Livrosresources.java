025da1d64533ef3e192c64165a8f6f6835e6105d
italo <italo.haaspascoli@gmail.com>
2020-02-28 14:39:52 -0300

    Adicionado metodo push put e delete

--- src/main/java/com/gft/socialbooks/resources/Livrosresources.java
+++ src/main/java/com/gft/socialbooks/resources/Livrosresources.java
@@ -1,12 +1,17 @@
 package com.gft.socialbooks.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gft.socialbooks.domain.Livro;
 import com.gft.socialbooks.repository.LivrosRep;
@@ -27,5 +32,32 @@ public class Livrosresources {
 		public void salvar(@RequestBody Livro livro)
 		{
 			livros.save(livro);
+			
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(livro.getId()).toUri();
 		}
+			
+		@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+		public void deletar(@PathVariable("id") Long id)
+		{
+			livros.deleteById(id);
+		}
+		
+		@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+		public void editar(@RequestBody Livro livro, @PathVariable("id") Long id)
+		{
+			livro.setId(id);
+			livros.save(livro);
+		}
+		
+		@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+		public ResponseEntity<?> buscar(@PathVariable("id") Long id) {
+			Livro livro = livros.findById(id).get();
+			
+			if(livro == null)
+			{
+				return ResponseEntity.notFound().build();
+			}
+			
+			return ResponseEntity.status(HttpStatus.OK).body(livro);
+		}		
 }
